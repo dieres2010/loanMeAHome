694777563df1ac4b4c52f0e3c761825eebf7e52a
Chris Waters <floridarealty001@gmail.com>
2022-04-07 12:11:00 -0400

    updated seeds

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -3,21 +3,54 @@ const { User, Post } = require('../models');
 
 const userdata = [
   {
-    username: 'alesmonde0',
-    email: 'nwestnedge0@cbc.ca',
+    username: 'Brian Ha',
+    email: 'BrianHa@stratton.com',
     password: 'password123',
-    license_number: '1234567',
-    phone_number: '8504441212',
-    mortgage_name: 'Tally Loans'
+    license_number: 'SL0000',
+    phone_number: '9735665653',
+    mortgage_name: 'Stratton Equities'
   },
   {
-    username: 'jwilloughway1',
-    email: 'rmebes1@sogou.com',
+    username: 'Caitlin Yancey Moore',
+    email: 'Caitlin@TC.com',
     password: 'password123',
-    license_number: '0987654',
-    phone_number: '8505551245',
-    mortgage_name: 'Noles Mortgage'
+    license_number: '1563244',
+    phone_number: '8505101169',
+    mortgage_name: 'TC Federal Bank'
+  },
+  {
+    username: 'Micheal Forslund',
+    email: 'Micheal@FIMC.com',
+    password: 'password123',
+    license_number: '2289',
+    phone_number: '8509804400',
+    mortgage_name: 'TC Federal Bank'
+  },
+{
+    username: 'Josh Keller',
+    email: 'Josh@GuaranteedRate.com',
+    password: 'password123',
+    license_number: '1570183',
+    phone_number: '8508161838',
+    mortgage_name: 'TC Federal Bank'
+  },
+  {
+    username: 'Omar Correa',
+    email: 'Omar@mmgloans.com',
+    password: 'password123',
+    license_number: '420420',
+    phone_number: '8501304400',
+    mortgage_name: 'MMG Mortgage Corps'
+  },
+  {
+    username: 'Ben Faint',
+    email: 'Ben@farmcredit.com',
+    password: 'password123',
+    license_number: 'NMLS1574721',
+    phone_number: '8505574140',
+    mortgage_name: 'Farm Credit' 
   }
+
  
 ];
 
